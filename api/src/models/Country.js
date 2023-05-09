8801c2d2753f6560f31104dccda78ecd9e2f4cf6
unknown <matias2k00@gmail.com>
2023-05-08 21:26:04 -0300

    modelo country finaliado

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,40 +3,44 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
-    id: {
-      type: DataTypes.STRING(3),
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    'country',
+    {
+      id: {
+        type: DataTypes.STRING(3),
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      flagImage: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      continente: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      poblacion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    flagImage: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
-    continente: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    area: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    poblacion: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
