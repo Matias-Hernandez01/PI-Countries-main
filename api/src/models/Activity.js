19ba39169bda7c8598cb0962696cfdb553f849d5
unknown <matias2k00@gmail.com>
2023-05-08 21:23:23 -0300

    modelo finalizado activity

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,40 +1,44 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('activity', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
+  sequelize.define(
+    'activity',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
 
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-    dificultad: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 1,
-        max: 5,
+      dificultad: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+        },
+      },
+
+      duracion: {
+        type: DataTypes.DECIMAL(4, 1),
+        allowNull: false,
+        validation: {
+          min: 0.1,
+          max: 999.9,
+        },
+      },
+
+      temporada: {
+        type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
+        allowNull: false,
       },
     },
-
-    duracion: {
-      type: DataTypes.DECIMAL(4, 1),
-      allowNull: false,
-      validation: {
-        min: 0.1,
-        max: 999.9,
-      },
-    },
-
-    temporada: {
-      type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera'),
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
