5bd7bb4b8c06df4e9c601ba27080a024ddb2d15b
unknown <matias2k00@gmail.com>
2023-05-10 04:28:25 -0300

    ruta de busqueda por id agregado, funciona correctamente

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const getAllCountries = require('./getAllCountries');
+const getCountrieById = require('./getCountrieById');
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/countries', getAllCountries);
+router.use('/countries/', getCountrieById);
 module.exports = router;
