ff7e3fd5c06b15907ce9a1d23d6383c10f4d68cd
unknown <matias2k00@gmail.com>
2023-05-10 04:26:06 -0300

    controlador que envia la respuesta del search by id

---
+++ api/src/controllers/countriesID/getCountriesById.js
@@ -0,0 +1,11 @@
+const countriesById = require('../countriesID/countriesById');
+
+module.exports = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resultado = await countriesById(id);
+    res.status(200).json(resultado);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
