98f787d33ab1130921c16011bed778f472636caa
unknown <matias2k00@gmail.com>
2023-05-12 02:47:39 -0300

    definiendo la ruta de busqueda por name

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 const getAllCountries = require('./getAllCountries');
 const getCountrieById = require('./getCountrieById');
+const getCountriByName = require('./getCountriByName');
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/countries', getAllCountries);
-router.use('/countries/', getCountrieById);
+router.use('/countries', getCountriByName);
+router.use('/countries', getCountrieById);
 module.exports = router;
