83ede7e500815802fd4dcadcf52a48b1943f8a08
unknown <matias2k00@gmail.com>
2023-05-12 05:45:20 -0300

    funciona, solo falta corregir las rutas

--- api/src/controllers/countriesByName/getCountriesName.js
+++ api/src/controllers/countriesByName/getCountriesName.js
@@ -2,10 +2,14 @@ const searchByName = require('./searchByName');
 
 module.exports = async (req, res) => {
   try {
-    const name = req.query.name;
+    const { name } = req.query;
     const results = await searchByName(name);
-    res.status(200).json(results);
+    name && results
+      ? res.status(200).json(results)
+      : res.status(404).json({ message: '' });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res
+      .status(400)
+      .json({ error: 'Error al buscar pais por nombre' + error.message });
   }
 };
