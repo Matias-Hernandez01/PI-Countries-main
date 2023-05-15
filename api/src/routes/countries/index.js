089e95530ade9c395e5f6ff7bbacd9269d29a8c7
unknown <matias2k00@gmail.com>
2023-05-14 23:23:19 -0300

    comentando

--- api/src/routes/countries/index.js
+++ api/src/routes/countries/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const CountriesController = require('../../controllers/countries');
 const router = Router();
-const controller = new CountriesController(); //*instancio la clase CountriesController, donde tengo todas las funciones de los controllers
+const controller = new CountriesController(); //*instancio la clase CountriesController, donde tengo todas las funciones
 
 //* Obtener todos los paises.
 router.get('/', controller.findAll);
