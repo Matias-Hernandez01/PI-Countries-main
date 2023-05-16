fafa15462d5352aa246eb8236da657a7da20fd72
unknown <matias2k00@gmail.com>
2023-05-15 23:41:38 -0300

    agregando ruta de activities a la ruta principal del PI

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const countryRouter = require('./countries');
+const activityRouter = require('./activities');
 
 router.use('/countries', countryRouter);
+router.use('/activities', activityRouter);
 
 module.exports = router;
