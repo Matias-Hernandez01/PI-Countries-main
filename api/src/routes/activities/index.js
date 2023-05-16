ba05821d503f0ccbdef3faac80553d87226911d6
unknown <matias2k00@gmail.com>
2023-05-15 23:41:08 -0300

    rutas de activities

---
+++ api/src/routes/activities/index.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const ActivityController = require('../../controllers/activities');
+const router = Router();
+const controller = new ActivityController();
+
+//*Ruta para crear actividad
+router.post('/', controller.createActivity);
+
+//*Ruta para recibir todas las actividades
+// router.get('/', controller.allActivities);
+
+module.exports = router;
