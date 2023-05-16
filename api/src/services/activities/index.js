8c76df7bb9f893c61ff030e95f82a83e7dac5031
unknown <matias2k00@gmail.com>
2023-05-16 02:52:39 -0300

    servicios de activities terminados

--- api/src/services/activities/index.js
+++ api/src/services/activities/index.js
@@ -3,16 +3,48 @@ const { Activity, Country } = require('../../db');
 class ActivityService {
   constructor() {}
 
-  async createActivity(name, dificultad, duracion, temporada, countries) {
-    const response = await Country.findAll();
-
-    const findCountries = await Country.findAll({
-      where: { name: countries },
+  //*handler que crea una nueva actividad
+  async createActivity(activities) {
+    const countries = activities[4];
+    const findCountries = await Country.findAll({ where: { name: countries } });
+    if (findCountries.length === 0) throw Error('No se encontraron paises');
+    const activitiesRepeat = await Activity.findAll({
+      where: {
+        name: activities[0],
+        dificultad: activities[1],
+        duracion: activities[2],
+        temporada: activities[3],
+      },
     });
 
-    console.log(findCountries);
+    if (activitiesRepeat.length) {
+      return { message: 'Ya existe la actividad que intenta crear' };
+    }
 
-    if (findCountries.length === O) throw Error('No se encontraron paises');
+    for (const country of findCountries) {
+      const newActivity = await Activity.create({
+        name: activities[0],
+        dificultad: activities[1],
+        duracion: activities[2],
+        temporada: activities[3],
+      });
+
+      await country.addActivity(newActivity);
+    }
+    if (findCountries.length > 1) {
+      return {
+        message: 'Actividad creada correctamente en los paises indicados',
+      };
+    } else if (findCountries.length === 1) {
+      return { message: 'Actividad creada correctamente en el pais indicado' };
+    }
+    throw new Error('Faltan datos para crear la actividad');
+  }
+
+  //*handlers que retorna todas las actividades creadas en mi BDD.
+  async allActivities() {
+    const activities = await Activity.findAll();
+    return activities;
   }
 }
 
