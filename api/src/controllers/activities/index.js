f7cb99c91c184f56d86a7b5bc637d199f716a6ce
unknown <matias2k00@gmail.com>
2023-05-16 02:51:09 -0300

    controllers de activities terminado

--- api/src/controllers/activities/index.js
+++ api/src/controllers/activities/index.js
@@ -9,18 +9,22 @@ class ActivityControllers {
   async createActivity(req, res) {
     await basesDatos.checkingBDD();
     const { name, dificultad, duracion, temporada, countries } = req.body;
-    // const activity = [name, dificultad, duracion, temporada, countries];
+    const activity = [name, dificultad, duracion, temporada, countries];
     try {
-      const response = await service.createActivity(
-        name,
-        dificultad,
-        duracion,
-        temporada,
-        countries
-      );
+      const response = await service.createActivity(activity);
       res.status(200).json(response);
     } catch (error) {
       res.status(404).json({ error: error.message });
+    }
+  }
+
+  async allActivities(req, res) {
+    await basesDatos.checkingBDD();
+    try {
+      const allActivities = await service.allActivities();
+      res.status(200).json(allActivities);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
   }
 }
