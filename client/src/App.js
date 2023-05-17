66e1f4d3df259330d3c3c7e48e02d684a13dcd5d
unknown <matias2k00@gmail.com>
2023-05-17 10:02:30 -0300

    modificacion de app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import { Route } from 'react-router-dom';
+import Home from './views/Home/Home';
+import Landing from './views/Landing/Landing';
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
+    <div className='App'>
+      <Route exact path='/' Component={<Landing />}>
+        <Landing />
+      </Route>
+      <Route path='/home'>
+        <Home />
+      </Route>
     </div>
   );
 }
