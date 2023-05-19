6220142494a9e763ec2e3628e790e499073f34d9
unknown <matias2k00@gmail.com>
2023-05-18 22:29:11 -0300

    home component

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,4 +1,17 @@
+import CardsContainer from '../../components/CardsContainer/CardsContainer';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getApi } from '../../redux/actions/';
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getApi());
+  }, [dispatch]);
+
+  return (
+    <>
+      <CardsContainer />
+    </>
+  );
 };
 export default Home;
