96e5b01ee976cc42ff06da017e46126ec893a742
unknown <matias2k00@gmail.com>
2023-05-18 22:24:55 -0300

    cardContainer, paginacion agregado

---
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from '../Card/Card';
+import style from './CardsContainer.module.css';
+import { useState } from 'react';
+import Paginado from '../Paginado/Paginado';
+
+const CardsContainer = () => {
+  const getInfo = useSelector((state) => state.allCountries);
+  //*Pagina actual
+  const [currentPage, setCurrentPage] = useState(1);
+  //*Paises por pagina => 10 en mi caso
+  const [countryPerPage, setCountryPer] = useState(12);
+  //*Indice del ultimo pais, multiplicando pagina actual * paises por pagina.
+  const indexOfLastCountry = currentPage * countryPerPage;
+  //*Indice del primer pais, multiplicando pagina
+  const indexOfFirstCountry = indexOfLastCountry - countryPerPage;
+
+  const currentCountries = getInfo.slice(
+    indexOfFirstCountry,
+    indexOfLastCountry
+  );
+
+  const paginado = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <>
+      <div className={style.mainContainer}>
+        <div className={style.paginado}>
+          <Paginado
+            countriesPerPage={countryPerPage}
+            allCountries={getInfo.length}
+            paginado={paginado}
+          />
+        </div>
+        <div>FILTRADO</div>
+        <div className={style.cards}>
+          {currentCountries?.map((country, index) => {
+            return (
+              <div key={index}>
+                <Card
+                  name={country.name}
+                  image={country.flagImage}
+                  continent={country.continent}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+export default CardsContainer;
