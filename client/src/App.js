e5d79c3b7c45ffa7568dd48cf474b09a25181717
unknown <matias2k00@gmail.com>
2023-05-18 22:23:33 -0300

    modificado

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Route } from 'react-router-dom';
-import Home from './views/Home/Home';
-import Landing from './views/Landing/Landing';
+import { Landing, Home, Form } from './views';
+import NavBar from './components/NavBar/NavBar';
+import { useLocation } from 'react-router-dom';
 function App() {
+  const location = useLocation();
   return (
     <div className='App'>
-      <Route exact path='/' Component={<Landing />}>
-        <Landing />
-      </Route>
-      <Route path='/home'>
-        <Home />
-      </Route>
+      {location.pathname !== '/' && <NavBar />}
+      <Route exact path='/' component={Landing} />
+      <Route path='/home' component={Home} />
+      <Route exact path='/create' component={Form} />
     </div>
   );
 }
