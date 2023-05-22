1f182c508e1dae875884ee924631068894699752
unknown <matias2k00@gmail.com>
2023-05-21 23:34:44 -0300

    componente de card finalizado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,9 +3,11 @@ import style from './Card.module.css';
 const Card = (props) => {
   return (
     <div className={style.card}>
-      <img src={props.image} alt={'Bandera de' + props.name} />
-      <h2>{props.name}</h2>
-      <h3>{props.continent}</h3>
+      <div className={style.containerImage}>
+        <img className={style.imagen} src={props.image} alt='bandera' />
+      </div>
+      <h2 className={style.name}>{props.name}</h2>
+      <h3 className={style.continent}>{props.continent}</h3>
     </div>
   );
 };
