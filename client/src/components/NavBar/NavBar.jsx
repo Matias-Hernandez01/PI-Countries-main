75a73578eb7f87b88f09667feba6b0ecb56b3448
unknown <matias2k00@gmail.com>
2023-05-21 23:36:41 -0300

    navbar, falta darle estilos al boton para crear activities

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,24 @@
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import style from './NavBar.module.css';
 import logo from '../../images/mundoLogo.png';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchByName } from '../../redux/actions/index';
+import image from '../../images/lupa.png';
 const NavBar = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const [valueSearch, setValueSearch] = useState('');
+
+  const handleChangeSearch = (event) => {
+    const value = event.target.value;
+    setValueSearch({ ...valueSearch, valueSearch: value });
+  };
+
+  const search = () => {
+    dispatch(searchByName(valueSearch));
+    setValueSearch({ ...valueSearch, valueSearch: '' });
+  };
 
   return (
     <div className={style.containerNavBar}>
@@ -18,13 +34,19 @@ const NavBar = () => {
       </div>
       <div className={style.containerSearchBar}>
         <div className={style.buttonAndInput}>
-          <button>Search</button>
-          <input></input>
+          <img className={style.lupa} src={image} onClick={search} />
+        </div>
+        <div className={style.inputSearch}>
+          <input
+            onChange={handleChangeSearch}
+            placeholder='Search country'
+          ></input>
         </div>
       </div>
 
       <div className={style.containerCreate}>
         <h4
+          className={style.create}
           onClick={() => {
             history.push('/create');
           }}
