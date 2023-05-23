e02df57399de3948f2f2275ba52877e966879e50
unknown <matias2k00@gmail.com>
2023-05-23 05:57:14 -0300

    componente activities terminado

---
+++ client/src/components/Activities/Activities.jsx
@@ -0,0 +1,48 @@
+import style from './Activities.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useEffect } from 'react';
+import { getActivities } from '../../redux/actions';
+
+const Activities = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const allActivities = useSelector((state) => state.activities);
+
+  useEffect(() => {
+    dispatch(getActivities());
+  }, [dispatch]);
+  console.log(allActivities);
+
+  return (
+    <div className={style.mainContainer}>
+      <div className={style.containerActivity}>
+        {allActivities
+          ? allActivities.map((activity, index) => {
+              return (
+                <div className={style.activity} key={index}>
+                  <div className={style.name}>
+                    <h2>Name:</h2>
+                    <h1>{activity.name}</h1>
+                  </div>
+                  <div className={style.dificultad}>
+                    <h2>Dificultad:</h2>
+                    <h1>{activity.dificultad}</h1>
+                  </div>
+                  <div className={style.duracion}>
+                    <h2>Duracion:</h2>
+                    <h1>{activity.duracion}</h1>
+                  </div>
+                  <div className={style.temporada}>
+                    <h2>Temporada:</h2>
+                    <h1>{activity.temporada}</h1>
+                  </div>
+                </div>
+              );
+            })
+          : ''}
+      </div>
+    </div>
+  );
+};
+export default Activities;
