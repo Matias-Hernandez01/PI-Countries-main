16133be1bb235bc3d01ca88b4259f1067094ebc6
unknown <matias2k00@gmail.com>
2023-05-22 23:42:49 -0300

    falta arreglar filtrado por activity

--- client/src/redux/actions/type/index.js
+++ client/src/redux/actions/type/index.js
@@ -2,3 +2,4 @@ export const GET_API = 'GET_API';
 export const GET_COUNTRY = 'GET_COUNTRY';
 export const FILTER = 'FILTER';
 export const SEARCH = 'SEARCH';
+export const COUNTRY_DETAIL = 'COUNTRY_DETAIL';
