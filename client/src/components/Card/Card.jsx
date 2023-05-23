83cc4fd88461d7b51130e62c7ab08f3ee2ab2dc6
unknown <matias2k00@gmail.com>
2023-05-22 23:36:49 -0300

    card terminado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
 import style from './Card.module.css';
-
+import { Link } from 'react-router-dom';
 const Card = (props) => {
   return (
     <div className={style.card}>
-      <div className={style.containerImage}>
-        <img className={style.imagen} src={props.image} alt='bandera' />
-      </div>
-      <h2 className={style.name}>{props.name}</h2>
-      <h3 className={style.continent}>{props.continent}</h3>
+      <Link className={style.link} to={`/detail/${props.id}`}>
+        <div className={style.containerImage}>
+          <img className={style.imagen} src={props.image} alt='bandera' />
+        </div>
+        <h2 className={style.name}>{props.name}</h2>
+        <h3 className={style.continent}>{props.continent}</h3>
+      </Link>
     </div>
   );
 };
