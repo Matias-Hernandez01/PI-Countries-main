ae820c2c8118b196e7469ff85987750c6faaa794
unknown <matias2k00@gmail.com>
2023-05-23 05:56:27 -0300

    app terminado

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,28 @@ import { Route } from 'react-router-dom';
 import { Landing, Home, Form, Detail } from './views';
 import NavBar from './components/NavBar/NavBar';
 import { useLocation } from 'react-router-dom';
+import Bienvenida from './views/Bienvenida/Bienvenida';
+import Activities from './components/Activities/Activities';
 
 function App() {
   const location = useLocation();
   return (
     <div className='App'>
-      {location.pathname !== '/' && <NavBar />}
+      {location.pathname !== '/' && location.pathname !== '/bienvenida' && (
+        <NavBar />
+      )}
+      <Route exact path='/activities' component={Activities} />
+      <Route exact path='/bienvenida' component={Bienvenida} />
       <Route exact path='/' component={Landing} />
       <Route path='/home' component={Home} />
       <Route exact path='/create' component={Form} />
       <Route exact path='/detail/:id' component={Detail} />
+      {location.pathname !== '/bienvenida' &&
+        location.pathname !== '/bienvenida' &&
+        location.pathname !== '/activities' &&
+        location.pathname !== '/home' &&
+        location.pathname !== '/create' &&
+        location.pathname !== '/detail/:id' && <Home />}
     </div>
   );
 }
