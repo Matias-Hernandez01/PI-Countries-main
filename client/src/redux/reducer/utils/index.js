ab6a50222ce0377cf7a52c34d63f56f054435383
unknown <matias2k00@gmail.com>
2023-05-23 06:01:29 -0300

    utils reducer

--- client/src/redux/reducer/utils/index.js
+++ client/src/redux/reducer/utils/index.js
@@ -1,53 +1,44 @@
 const applyFilter = (data, continent, population) => {
-  if (continent) {
-    const results = data.filter((country) => {
-      if (continent === 'Activity') {
-        return country.activities;
-      } else {
-        return country.continent === continent;
-      }
-    });
+  const results = data.filter((country) => country.continent === continent);
 
-    if (population) {
-      switch (population) {
-        case 'A-Z': {
-          results.sort((a, b) => {
-            if (a.name < b.name) return -1;
-            if (a.name > b.name) return 1;
-            return 0;
-          });
-          break; // Agregar break aquí
-        }
-        case 'Z-A': {
-          results.sort((a, b) => {
-            if (a.name < b.name) return 1;
-            if (a.name > b.name) return -1;
-            return 0;
-          });
-          break; // Agregar break aquí
-        }
-        case '+': {
-          results.sort((a, b) => {
-            if (a.poblacion < b.poblacion) return 1;
-            if (a.poblacion > b.poblacion) return -1;
-            return 0;
-          });
-          break; // Agregar break aquí
-        }
-        case '-': {
-          results.sort((a, b) => {
-            if (a.poblacion < b.poblacion) return -1;
-            if (a.poblacion > b.poblacion) return 1;
-            return 0;
-          });
-          break; // Agregar break aquí
-        }
-        default: {
-          return;
-        }
+  if (population) {
+    switch (population) {
+      case 'A-Z': {
+        results.sort((a, b) => {
+          if (a.name < b.name) return -1;
+          if (a.name > b.name) return 1;
+          return 0;
+        });
+        break; // Agregar break aquí
+      }
+      case 'Z-A': {
+        results.sort((a, b) => {
+          if (a.name < b.name) return 1;
+          if (a.name > b.name) return -1;
+          return 0;
+        });
+        break; // Agregar break aquí
+      }
+      case '+': {
+        results.sort((a, b) => {
+          if (a.poblacion < b.poblacion) return 1;
+          if (a.poblacion > b.poblacion) return -1;
+          return 0;
+        });
+        break; // Agregar break aquí
+      }
+      case '-': {
+        results.sort((a, b) => {
+          if (a.poblacion < b.poblacion) return -1;
+          if (a.poblacion > b.poblacion) return 1;
+          return 0;
+        });
+        break; // Agregar break aquí
+      }
+      default: {
+        return;
       }
     }
-
     return results;
   }
 
