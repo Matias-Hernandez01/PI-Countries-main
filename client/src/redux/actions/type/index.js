3e72cec482411560db22a3fbd7155f987f197f67
unknown <matias2k00@gmail.com>
2023-05-24 23:06:11 -0300

    action de filter por activitie

--- client/src/redux/actions/type/index.js
+++ client/src/redux/actions/type/index.js
@@ -4,3 +4,4 @@ export const FILTER = 'FILTER';
 export const SEARCH = 'SEARCH';
 export const COUNTRY_DETAIL = 'COUNTRY_DETAIL';
 export const GET_ACTIVITIES = 'GET_ACTIVITIES';
+export const FILTER_ACTIVITIES = 'FILTER_ACTIVITIES';
