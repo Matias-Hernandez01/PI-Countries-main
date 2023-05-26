3b68cc5240d4e3b29554b75276bb66bafc758c7a
unknown <matias2k00@gmail.com>
2023-05-25 23:49:53 -0300

    detail terminado

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,7 +1,9 @@
 import DetailCard from '../../components/DetailCard/DetailCard';
+import NavBar from '../../components/NavBar/NavBar';
 const Detail = () => {
   return (
     <div>
+      <NavBar />
       <DetailCard />
     </div>
   );
