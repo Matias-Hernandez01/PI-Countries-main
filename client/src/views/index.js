d11990adfef667c83e247821c5c32895157a2970
unknown <matias2k00@gmail.com>
2023-05-25 23:43:39 -0300

    views terminadas

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -2,5 +2,6 @@ import Landing from './Landing/Landing';
 import Home from './Home/Home';
 import Detail from './Detail/Detail';
 import Form from './Form/Form';
-
-export { Landing, Home, Detail, Form };
+import NotFound from './NotFound/NotFound';
+import Bienvenida from './Bienvenida/Bienvenida';
+export { Landing, Home, Detail, Form, NotFound, Bienvenida };
