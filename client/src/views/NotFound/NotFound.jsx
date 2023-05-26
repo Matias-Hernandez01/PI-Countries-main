efd5c41b1da6b3df12db86f955e7ef95c5df720b
unknown <matias2k00@gmail.com>
2023-05-25 23:48:45 -0300

    not found terminado

--- client/src/views/NotFound/NotFound.jsx
+++ client/src/views/NotFound/NotFound.jsx
@@ -1,12 +1,23 @@
 import notFound from '../../images/notFound.png';
 import style from './NotFound.module.css';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
 const NotFound = () => {
+  const history = useHistory();
   return (
     <div>
-      <h1>Not Found</h1>
+      <h1 className={style.notFound}>Not Found</h1>
       <div>
-        <img src={notFound} alt='mundo'></img>
+        <img className={style.image} src={notFound} alt='mundo'></img>
       </div>
+      <button
+        className={style.volverAlHome}
+        onClick={() => {
+          history.push('/home');
+        }}
+      >
+        Volver al home
+      </button>
     </div>
   );
 };
