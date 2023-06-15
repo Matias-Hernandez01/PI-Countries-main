b21652c0c6f212bb8c5b9de30101c10cf926866d
unknown <matias2k00@gmail.com>
2023-06-15 01:08:58 -0300

    agregando funcionalidad de loading a mi home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,12 +1,21 @@
 import CardsContainer from '../../components/CardsContainer/CardsContainer';
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
-
+import Loading from '../../components/Loading/Loading';
+import { useState } from 'react';
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
-      <NavBar />
-      <CardsContainer />
+      {loading === true ? (
+        <Loading setLoading={setLoading} />
+      ) : (
+        <div>
+          <NavBar />
+          <CardsContainer />
+        </div>
+      )}
     </>
   );
 };
